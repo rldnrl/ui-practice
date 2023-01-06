f07fea52785536a5f126f812eaf210e4206158d9
rldnrl <devpark1002@gmail.com>
2023-01-06 18:29:18 +0900

    style: style select menu component

---
+++ react/src/ui/SelectMenu/SelectMenu.style.tsx
@@ -0,0 +1,53 @@
+import { css } from "@emotion/css";
+
+const selectMenuStyle = {
+  selectMenu: css`
+    width: 380px;
+    margin: 0 auto;
+  `,
+  selectButton: css`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
+    height: 55px;
+    background-color: white;
+    padding: 20px;
+    font-size: 18px;
+    font-weight: 400;
+    border-radius: 8px;
+    cursor: pointer;
+  `,
+  icon: (active: boolean) => css`
+    font-size: 25px;
+    transition: 0.3s;
+    ${active ? "transform: rotate(-180deg);" : ""}
+  `,
+  options: (active: boolean) => css`
+    position: relative;
+    padding: 20px;
+    margin-top: 10px;
+    border-radius: 8px;
+    background-color: white;
+    box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.1);
+    display: ${active ? "block" : "none"};
+  `,
+  option: css`
+    display: flex;
+    align-items: center;
+    height: 50px;
+    cursor: pointer;
+    padding: 0 16px;
+    border-radius: 8px;
+    background-color: white;
+    &:hover {
+      background-color: #f2f2f2;
+    }
+  `,
+  optionText: css`
+    font-size: 18px;
+    color: #333;
+  `,
+};
+
+export default selectMenuStyle;
