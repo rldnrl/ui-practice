f0f8354b0ed858c087d0ba161362f6ecd3459c48
rldnrl <devpark1002@gmail.com>
2023-01-06 18:30:40 +0900

    feat: implement example for select menu component

---
+++ react/src/example/ExampleSelectMenu.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { Field } from "../types/field";
+import SelectMenu from "../ui/SelectMenu";
+
+const fields: Field[] = [
+  {
+    value: "github",
+    label: "Github",
+  },
+  {
+    value: "instagram",
+    label: "Instagram",
+  },
+  {
+    value: "youtube",
+    label: "Youtube",
+  },
+  {
+    value: "leetcode",
+    label: "LeetCode",
+  },
+];
+
+export default function ExampleSelectMenu() {
+  const [selectedField, setSelectedField] = useState<Field>();
+
+  const handleSelect = (field?: Field) => {
+    setSelectedField(field);
+  };
+
+  return (
+    <SelectMenu
+      fields={fields}
+      selectedField={selectedField}
+      onSelect={handleSelect}
+    />
+  );
+}
