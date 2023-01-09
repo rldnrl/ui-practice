bd436b4f8bbcbd842014dde513c089614e84d5fb
rldnrl <devpark1002@gmail.com>
2023-01-09 22:22:27 +0900

    feat: implement menu item component

---
+++ react/src/compound-components/MenuItem/MenuItem.tsx
@@ -0,0 +1,30 @@
+import { MouseEventHandler, ReactNode } from "react";
+import menuItemStyle from "./MenuItem.style";
+import { useSelectMenu } from "../SelectMenu/SelectMenu";
+
+interface MenuItemProps {
+  value: string;
+  children: ReactNode;
+}
+
+export default function MenuItem({ value, children }: MenuItemProps) {
+  const { onChange, setInnerLabel } = useSelectMenu();
+
+  const handleClick: MouseEventHandler<HTMLElement> = (e) => {
+    if (!(e.target instanceof HTMLElement)) {
+      return;
+    }
+    onChange?.(e.target.dataset.value);
+    setInnerLabel(e.target.innerText);
+  };
+
+  return (
+    <li
+      data-value={value}
+      className={menuItemStyle.option}
+      onClick={handleClick}
+    >
+      <span className={menuItemStyle.optionText}>{children}</span>
+    </li>
+  );
+}
