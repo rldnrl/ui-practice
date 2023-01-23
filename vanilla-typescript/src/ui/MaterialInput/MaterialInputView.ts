dc78942e5c46db337fe32aab26bd2d5367f5c58e
rldnrl <devpark1002@gmail.com>
2023-01-23 18:40:18 +0900

    feat: mark up material input

---
+++ vanilla-typescript/src/ui/MaterialInput/MaterialInputView.ts
@@ -0,0 +1,13 @@
+import "./style.css";
+
+const MaterialInputView = `
+  <div class="container">
+    <div class="input-box">
+      <input type="text" required>
+      <label>Password</label>
+      <i class='bx bx-hide'></i>
+    </div>
+  </div>
+`;
+
+export default MaterialInputView;
