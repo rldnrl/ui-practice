81f2c13f11ce545b078a8559b79e6eb589b91d09
rldnrl <devpark1002@gmail.com>
2023-01-23 22:05:05 +0900

    feat: apply material input component

--- vanilla-typescript/src/main.ts
+++ vanilla-typescript/src/main.ts
@@ -1,8 +1,12 @@
-import "./style.css";
-import MaterialInputView from "./ui/MaterialInput/MaterialInputView";
 import SelectMenuController from "./ui/SelectMenu/SelectMenuController";
 import SelectMenuView from "./ui/SelectMenu/SelectMenuView";
+
+import MaterialInputView from "./ui/MaterialInput/MaterialInputView";
+import MaterialInputController from "./ui/MaterialInput/MaterialInputController";
+
+import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML += SelectMenuView;
 document.querySelector<HTMLDivElement>("#app")!.innerHTML += MaterialInputView;
 SelectMenuController();
+MaterialInputController();
