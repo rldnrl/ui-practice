ad219249c46623d0042d72df0e8fb2adbcbe88cd
rldnrl <devpark1002@gmail.com>
2023-01-30 20:52:28 +0900

    feat: apply example code

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,11 +1,15 @@
 import ExampleSelectMenu from "./example/ui/ExampleSelectMenu";
 import ExampleSelectMenuCompoundComponent from "./example/compound-components/ExampleSelectMenu";
+import ExampleMaterialInput from "./example/ui/ExampleMaterialInput";
+import ExampleStopwatch from "./example/ui/ExampleStopwatch";
 
 function App() {
   return (
     <>
       <ExampleSelectMenu />
       <ExampleSelectMenuCompoundComponent />
+      <ExampleMaterialInput />
+      <ExampleStopwatch />
     </>
   );
 }
